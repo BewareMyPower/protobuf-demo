5438b184bc8c7067b7a857b11abd3bcf134fac8c
Yunze Xu <xyzinfernity@163.com>
2021-07-15 14:37:05 +0800

    Use Pulsar's test proto files

--- examples/hex_dump.h
+++ examples/hex_dump.h
@@ -3,20 +3,38 @@
 #include <stdio.h>
 
 #include <sstream>
+#include <vector>
 
 inline auto dumpBytes(const char* data, size_t size,
                       size_t count_per_line = 8) {
   std::ostringstream os;
+  std::vector<int> line;
+
   for (size_t i = 0; i < size; i++) {
     char buf[3];
     snprintf(buf, sizeof(buf), "%02x", static_cast<unsigned char>(data[i]));
+    line.emplace_back(static_cast<int>(data[i]));
+
     if (i % count_per_line != 0) {
       os << " ";
     }
     os << buf;
     if ((i + 1) % count_per_line == 0) {
+      os << " |";
+      for (int x : line) {
+        os << " " << x;
+      }
       os << "\n";
+      line.clear();
     }
+  }
+
+  if (!line.empty()) {
+    os << std::string((count_per_line - line.size()) * 3 + 1, ' ') << "|";
+    for (int x : line) {
+      os << " " << x;
+    }
+    os << "\n";
   }
   return os.str();
 }
--- examples/schema.cc
+++ examples/schema.cc
@@ -3,47 +3,38 @@
 #include <google/protobuf/descriptor.pb.h>
 
 #include <iostream>
-#include <set>
 #include <vector>
 
+#include "Test.pb.h"
 #include "hex_dump.h"
-#include "pulsar_message.pb.h"
 
 using google::protobuf::FileDescriptor;
 using google::protobuf::FileDescriptorSet;
 using std::cout;
 
-void internalSerializeFileDescriptor(const FileDescriptor* file_descriptor,
-                                     FileDescriptorSet& file_descriptor_set,
-                                     std::set<std::string>& cache) {
-  if (cache.find(file_descriptor->name()) != cache.cend()) {
-    return;
-  }
-  for (int i = 0; i < file_descriptor->dependency_count(); i++) {
-    const auto dependency = file_descriptor->dependency(i);
-    if (cache.find(dependency->name()) == cache.cend()) {
-      internalSerializeFileDescriptor(dependency, file_descriptor_set, cache);
-    }
-  }
+void internalCollectFileDescriptors(const FileDescriptor* file_descriptor,
+                                    FileDescriptorSet& file_descriptor_set) {
   file_descriptor->CopyTo(file_descriptor_set.add_file());
-  // mark current file_descriptor as added to avoid repeated class name
-  cache.emplace(file_descriptor->name());
+  for (int i = 0; i < file_descriptor->dependency_count(); i++) {
+    // collect the file descriptors recursively
+    internalCollectFileDescriptors(file_descriptor->dependency(i),
+                                   file_descriptor_set);
+  }
 }
 
-FileDescriptorSet serializeFileDescriptor(
+FileDescriptorSet collectFileDescriptors(
     const FileDescriptor* file_descriptor) {
   FileDescriptorSet file_descriptor_set;
-  std::set<std::string> cache;
-  internalSerializeFileDescriptor(file_descriptor, file_descriptor_set, cache);
+  internalCollectFileDescriptors(file_descriptor, file_descriptor_set);
   return file_descriptor_set;
 }
 
 int main(int argc, char* argv[]) {
-  const auto descriptor = pulsar::TestMessage::GetDescriptor();
+  const auto descriptor = proto::TestMessage::GetDescriptor();
   const auto file_descriptor = descriptor->file();
 
   const FileDescriptorSet file_descriptor_set =
-      serializeFileDescriptor(file_descriptor);
+      collectFileDescriptors(file_descriptor);
   const auto root_message_type_name = descriptor->full_name();
   const auto root_file_descriptor_name = descriptor->file()->name();
 
